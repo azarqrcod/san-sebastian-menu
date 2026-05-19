59b7730514a617d090154f2473115eb179929ccb
menue <azarqrcod@gmail.com>
2026-05-20 00:49:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-console.log("San Sebastian System Loaded");
+const pages = [
+  "/san-sebastian-menu/hot/",
+  "/san-sebastian-menu/cold/",
+  "/san-sebastian-menu/cake/"
+];
 
-setInterval(() => {
-    location.reload();
-}, 3600000);
+const current = window.location.pathname;
+
+let index = pages.indexOf(current);
+
+if (index === -1) {
+  index = 0;
+}
+
+setTimeout(() => {
+
+  let next = index + 1;
+
+  if (next >= pages.length) {
+    next = 0;
+  }
+
+  window.location.href = pages[next];
+
+}, 15000);
